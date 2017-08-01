34267662b196b1cb23587bd9bfd01754736dc9f0
labrick <yananback@gmail.com>
2017-08-01 16:59:10 +0800

    solve 004 proble using merge idea

--- 004.Median-Of-Two-Sorted-Arrays/main.cpp
+++ 004.Median-Of-Two-Sorted-Arrays/main.cpp
@@ -32,7 +32,7 @@ public:
         }
     }
     // 允许重复
-    double findMedianSortedArray(vector<int> & nums1, vector<int> & nums2){ 
+    double findMedianSortedArray2(vector<int> & nums1, vector<int> & nums2){ 
         vector<int> nums(nums1);
         nums.insert(nums.end(), nums2.begin(), nums2.end());
         sort(nums.begin(), nums.end());
@@ -51,6 +51,28 @@ public:
             // return (num1 + num2)/2;
             return ((double)*(nums.begin()+setNum/2-1) + (double)*(nums.begin()+setNum/2))/2;
         }
+    }
+    double findMedianSortedArray(vector<int> & nums1, vector<int> & nums2){ 
+        vector<int> mergeNums;
+        int nums1Index = 0, nums2Index = 0;
+        for(int i=0; i<(int)nums1.size()+(int)nums2.size(); i++){
+            // cout << nums1Index << ":"<< nums2Index << endl;
+            if(nums1Index >= (int)nums1.size())             mergeNums.push_back(nums2[nums2Index++]);
+            else if(nums2Index >= (int)nums2.size())        mergeNums.push_back(nums1[nums1Index++]);
+            else if(nums1[nums1Index] > nums2[nums2Index])  mergeNums.push_back(nums2[nums2Index++]);
+            else                                            mergeNums.push_back(nums1[nums1Index++]);
+        }
+        // cout << nums1Index << ":"<< nums2Index << endl;
+        // for(int a : mergeNums)
+        //     cout << a << "\t";
+        // cout << "size: " << mergeNums.size() << endl;
+
+        int setNum = mergeNums.size();
+        if(setNum % 2){
+            return *(mergeNums.begin()+setNum/2);
+        } else {
+            return ((double)*(mergeNums.begin()+setNum/2-1) + (double)*(mergeNums.begin()+setNum/2))/2;
+        } 
     }
 };
 
